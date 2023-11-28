397ed5cdaa066b684d575401e88ba771392f32e1
James Parkes [sc222jp] <sc222jp@feng-linux-01.leeds.ac.uk>
2023-11-28 20:59:48 +0000

    Created relational database. Imported flask login

--- app/models.py
+++ app/models.py
@@ -4,14 +4,30 @@ from app import db
 class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     position = db.Column(db.Integer)
-    trackname = db.Column(db.String(500), index=True, unique=True)
+    trackname = db.Column(db.String(500))
     runtime = db.Column(db.Integer)
-    value = db.Column(db.Float)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'))
+
+
+favourites_association_table = db.Table(
+    "association_table",
+    db.Model.metadata,
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("album_id", db.ForeignKey("album.id"))
+)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(500))
+    password = db.Column(db.String(500))
+
 
 class Album(db.Model):
+    __tablename__ = "album"
     id = db.Column(db.Integer, primary_key=True)
-
-class Favourite(db.Model):
-
-class User(db.User):
-    
+    title = db.Column(db.String(500))
+    artist = db.Column(db.String(500))
+    year = db.Column(db.Integer)
+    tracks = db.relationship('Track', backref='album', lazy='dynamic')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash, request, redirect
-from app import app, db, models
+from app import app, db, models, login_manager
 from .forms import FlaskForm
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.User.get(user_id)
 
 @app.route('/')
 def home():
